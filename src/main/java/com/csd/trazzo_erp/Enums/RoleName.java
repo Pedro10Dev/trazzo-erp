0f4dc56a862fbb05d840df157cb31af6c8f45dea
Pedro Arellano <pedroarellano9810@gmail.com>
2026-05-09 17:23:47 -0400

    Tercer commit - Se crearon las tablas person,user y role

---
+++ src/main/java/com/csd/trazzo_erp/Enums/RoleName.java
@@ -0,0 +1,10 @@
+package com.csd.trazzo_erp.Enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_MANAGER,
+    ROLE_USER,
+    ROLE_CUSTOMER
+    
+
+}
--- src/main/java/com/csd/trazzo_erp/models/User.java
+++ src/main/java/com/csd/trazzo_erp/models/User.java
@@ -1,5 +1,54 @@
 package com.csd.trazzo_erp.models;
 
-public class User {
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class User extends BaseEntity{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false, name = "user_name")
+    private String userName;
+    
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled = true;
+
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "person_id", referencedColumnName = "id", nullable = false)
+    private Person person;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
     
 }
